cd487239b3a7f682a6b02487eb774b8464b8a12e
Gnecc <genecc4@gmail.com>
2019-05-22 23:26:05 -0500

    ya esta el filtro por area

--- src/app/pipes/filtro.pipe.ts
+++ src/app/pipes/filtro.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Maestro } from '../interfaces/interfaces';
 
 @Pipe({
   name: 'filtro'
 })
 export class FiltroPipe implements PipeTransform {
 
-  transform(arreglo: any[], texto: string): any[] {
+  transform(arreglo: any[], texto: string): Maestro[] {
 
     if (texto === '') {
       return arreglo;
@@ -14,7 +15,7 @@ export class FiltroPipe implements PipeTransform {
     texto = texto.toLowerCase();
 
     return arreglo.filter( maestro => {
-      return maestro.Nombre.toLowerCase()
+      return maestro.areaa.toLowerCase()
       .includes( texto);
     });
 
